956066584ef7cdfffb1ee431d8bce51e197b5d2a
barinov3d <barinov3d@gmail.com>
2017-05-25 09:02:13 +0300

    just a test comment2

--- app/src/main/java/com/barinov3dgmail/expensesandrevenues/MainActivity.java
+++ app/src/main/java/com/barinov3dgmail/expensesandrevenues/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 //just commentary for testing
+//just commentary for testing2
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
