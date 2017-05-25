f431dd595d26b94899d56ad9ea9306c0b6c71991
barinov3d <Beketova4215>
2017-05-25 21:34:31 +0300

    Adding coin images

--- app/src/main/java/com/barinov3dgmail/expensesandrevenues/MainActivity.java
+++ app/src/main/java/com/barinov3dgmail/expensesandrevenues/MainActivity.java
@@ -12,14 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        fragmentManager = getSupportFragmentManager();
+    }
 
-        runSplash();
-    }
-    public void runSplash(){
-        SplashFragment splashFragment = new SplashFragment();
-        fragmentManager.beginTransaction().replace(R.id.container, splashFragment).addToBackStack(null).commit() // в replace передаем идентификатор контейнера и отображаемый фрагмент
-        ; //транзакция отображения SplashScreen на экране
-    }
 }
 
