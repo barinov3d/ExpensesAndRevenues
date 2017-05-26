d167cfe0db1c26ed25f30b7ea7be88e6f9c58820
barinov3d <barinov3d@gmail.com>
2017-05-26 15:09:28 +0300

    adding fragmentManager1

--- app/src/main/java/com/barinov3dgmail/expensesandrevenues/MainActivity.java
+++ app/src/main/java/com/barinov3dgmail/expensesandrevenues/MainActivity.java
@@ -1,17 +1,30 @@
 package com.barinov3dgmail.expensesandrevenues;
 
-import android.support.v4.app.FragmentManager;
+import android.app.FragmentManager;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 
+import com.barinov3dgmail.expensesandrevenues.adapter.TabAdapter;
+
 public class MainActivity extends AppCompatActivity {
+
+    FragmentManager fragmentManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        fragmentManager = getFragmentManager();
+
+        setUI();
+
+
+}
 
     private void setUI() {
 
@@ -20,6 +33,14 @@ public class MainActivity extends AppCompatActivity {
             toolbar.setTitleTextColor(getResources().getColor(R.color.white));
             setSupportActionBar(toolbar);
         }
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.current_cost));
+        tabLayout.addTab(tabLayout.newTab().setText(R.string.done_costs));
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id .pager);
+        TabAdapter tabAdapter = new TabAdapter(fragmentManager, 2);
+
+
     }
 
 }
---
+++ app/src/main/java/com/barinov3dgmail/expensesandrevenues/adapter/TabAdapter.java
@@ -0,0 +1,41 @@
+package com.barinov3dgmail.expensesandrevenues.adapter;
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.support.v13.app.FragmentStatePagerAdapter;
+
+import com.barinov3dgmail.expensesandrevenues.fragment.CurrentCostFragment;
+import com.barinov3dgmail.expensesandrevenues.fragment.DoneCostFragment;
+
+/**
+ * Created by User on 26.05.2017.
+ */
+
+public class TabAdapter extends FragmentStatePagerAdapter {
+
+    private int numberOfTabs;
+
+    public TabAdapter(FragmentManager fragmentManager, int numberOfTubs) {
+        super(fragmentManager);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+       /* switch (position){
+            case 0:
+                return new CurrentCostFragment();
+            break;
+            case 1:
+                return new DoneCostFragment();
+            break;
+            default:
+            return null;
+        }*/
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return numberOfTabs;
+    }
+}
