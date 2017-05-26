88d6ca42f294f7057e8b94e145869cc3ddc3d315
barinov3d <barinov3d@gmail.com>
2017-05-26 11:13:07 +0300

    adding fragmentManager1

--- app/src/main/java/com/barinov3dgmail/expensesandrevenues/MainActivity.java
+++ app/src/main/java/com/barinov3dgmail/expensesandrevenues/MainActivity.java
@@ -3,6 +3,7 @@ package com.barinov3dgmail.expensesandrevenues;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +12,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    private void setUI() {
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if (toolbar != null) {
+            toolbar.setTitleTextColor(getResources().getColor(R.color.white));
+            setSupportActionBar(toolbar);
+        }
+
+
 }
 
