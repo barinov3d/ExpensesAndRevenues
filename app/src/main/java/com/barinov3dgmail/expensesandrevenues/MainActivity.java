bb80e3167a422eb51344750477340a6069438979
barinov3d <Beketova4215>
2017-05-26 22:37:23 +0300

    Adding coin images

--- app/src/main/java/com/barinov3dgmail/expensesandrevenues/MainActivity.java
+++ app/src/main/java/com/barinov3dgmail/expensesandrevenues/MainActivity.java
@@ -20,9 +20,10 @@ public class MainActivity extends AppCompatActivity
     FragmentManager fragmentManager;
 
     private int progress = 0;//
+    private int maxProgress = 0;
     private ProgressBar pbHorizontal;//
     private TextView tvProgressHorizontal;//
-
+    private TextView tvProgressMaxHorizontal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,19 +32,47 @@ public class MainActivity extends AppCompatActivity
 
         pbHorizontal = (ProgressBar) findViewById(R.id.pb_horizontal);//
         tvProgressHorizontal = (TextView) findViewById(R.id.tv_progress_horizontal);//
-                fragmentManager = getFragmentManager();
+        tvProgressMaxHorizontal = (TextView) findViewById(R.id.tv_max_progress_horizontal);
+        fragmentManager = getFragmentManager();
 
         setUI();
 
-
+//onClickPlus100
 }
-    public void onClick(View v) {//
+    public void onClick10(View v) {//
         progress = progress + 10;
         postProgress(progress);
+        maxProgress(maxProgress);
+    }
+    public void onClick100(View v) {//
+        progress = progress + 100;
+        postProgress(progress);
+        maxProgress(maxProgress);
+    }
+    public void onClick1000(View v) {//
+        progress = progress + 1000;
+        postProgress(progress);
+        maxProgress(maxProgress);
+    }
+    public void onClickPlus100(View v) {//
+        maxProgress = maxProgress + 100;
+        postProgress(progress);
+        maxProgress(maxProgress);
+    }
+    public void onClickPlus1000(View v) {//
+        maxProgress = maxProgress + 1000;
+        postProgress(progress);
+        maxProgress(maxProgress);
+    }
+    public void onClickPlus5000(View v) {//
+        maxProgress = maxProgress + 5000;
+        postProgress(progress);
+        maxProgress(maxProgress);
     }
 
     private void postProgress(int progress) {//
-        String strProgress = String.valueOf(progress) + " %";
+        String strProgress = String.valueOf(progress) + " руб.";
+        String strNegProgress = String.valueOf(progress-maxProgress) + " руб.";
         pbHorizontal.setProgress(progress);
 
         if (progress == 0) {
@@ -51,10 +80,16 @@ public class MainActivity extends AppCompatActivity
         } else {
             pbHorizontal.setSecondaryProgress(progress + 5);
         }
-        tvProgressHorizontal.setText(strProgress);
 
+        if (progress<=maxProgress)
+        tvProgressMaxHorizontal.setText(strProgress);
+        else tvProgressMaxHorizontal.setText("уходим в минус "+"-"+strNegProgress);
     }
-
+    private void maxProgress(int maxProgress) {//
+        String strProgress = String.valueOf(maxProgress) + " руб.";
+        tvProgressHorizontal.setText(strProgress);
+        pbHorizontal.setMax(maxProgress);
+    }
 
 
     private void setUI() {
